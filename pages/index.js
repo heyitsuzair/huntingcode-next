0f90af5423723fab9d840b86183d5f23dae4daff
heyitsuzair <uzairdevil354123@gmail.com>
2022-11-15 05:16:23 +0500

    Image Component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import Script from "next/script";
 import styles from "../styles/Home.module.css";
 
@@ -12,63 +13,42 @@ export default function Home() {
         <meta name="keywords" content="hunting coder,blog,nextjs" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Script src="/sc.js" strategy="lazyOnload" />
+      {/* <Script src="/sc.js" strategy="lazyOnload" /> */}
+
+      <nav className={styles.mainnav}>
+        <ul>
+          <li>Home</li>
+          <li>Blog</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next!</a>
+          Welcome to <Link href="/">Hunting Coder!</Link>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <p className={styles.description}>Hunting Developers Around World</p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className="blogs">
+            <h2>Popular Blogs</h2>
+            <div className="blogItem">
+              <h3>How To Learn JavaScript</h3>
+              <p>JavaScript Is The Web Language.</p>
+            </div>
+            <div className="blogItem">
+              <h3>How To Learn JavaScript</h3>
+              <p>JavaScript Is The Web Language.</p>
+            </div>
+            <div className="blogItem">
+              <h3>How To Learn JavaScript</h3>
+              <p>JavaScript Is The Web Language.</p>
+            </div>
+          </div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
