53d82a2b4f85cb1cbd3546983e7fc8b78cb5e198
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-11-16 01:13:44 +0500

    Single Blog Page Section Added

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,48 @@
+import Head from "next/head";
+import Link from "next/link";
 import React from "react";
+import styles from "../styles/Blog.module.css";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className={styles.container}>
+      <style jsx>
+        {`
+          h3 {
+            font-size: 28px;
+          }
+        `}
+      </style>
+      <Head>
+        <title>Blog</title>
+        <meta
+          name="description"
+          content="A Blog Website Created By Muhammad Uzair To Practice Next JS. Made With <3 By UZAIR"
+        />
+        <meta name="keywords" content="hunting coder,blog,nextjs" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.grid}>
+          <div>
+            <div className="blogItem dummy">
+              <Link href={"/blogpost/learn-js"}>
+                <h3>How To Learn JavaScript</h3>
+              </Link>
+              <p>JavaScript Is The Web Language.</p>
+            </div>
+            <div className="blogItem">
+              <h3>How To Learn JavaScript</h3>
+              <p>JavaScript Is The Web Language.</p>
+            </div>
+            <div className="blogItem">
+              <h3>How To Learn JavaScript</h3>
+              <p>JavaScript Is The Web Language.</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Blog;
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,12 +1,39 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 import React from "react";
+import styles from "../../styles/Blogpost.module.css";
 
 const Slug = () => {
   const router = useRouter();
 
   const { slug } = router.query;
 
-  return <div>{slug}</div>;
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{slug}</title>
+        <meta
+          name="description"
+          content="A Blog Website Created By Muhammad Uzair To Practice Next JS. Made With <3 By UZAIR"
+        />
+        <meta name="keywords" content="hunting coder,blog,nextjs" />
+      </Head>
+      <main className={styles.main}>
+        <h1>Title Of The Page {slug}</h1>
+        <hr className={styles.hr} />
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet in,
+          blanditiis quaerat, soluta laborum qui aliquam eos exercitationem
+          neque dolor non reiciendis, fugit cumque veniam quisquam? Libero
+          praesentium veniam in. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Nesciunt maxime in veritatis quo sed, voluptatibus
+          minima. Eaque maiores incidunt fugiat voluptatum vero dolores
+          voluptatem aperiam sed! Qui dicta amet suscipit ab, impedit esse rerum
+          maxime sapiente vel nisi eligendi omnis.
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Slug;
