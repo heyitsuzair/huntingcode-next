6a3b2f208d5e57242f3fd56d3f02570bc0d4506b
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-11-17 00:35:17 +0500

    postContact API Integerated

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,96 @@
-import React from "react";
-
+import React, { useState } from "react";
+import styles from "../styles/Contact.module.css";
+import axios from "axios";
+import { postContact } from "../utils/api";
 const Contact = () => {
-  return <div>Contact</div>;
+  const [name, setname] = useState();
+  const [email, setemail] = useState();
+  const [phone, setphone] = useState();
+  const [desc, setdesc] = useState();
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let apiData = {
+        name,
+        email,
+        phone,
+        desc,
+      };
+      const { data } = await axios.post(postContact, apiData);
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Contact Us</h1>
+      <form onSubmit={handleFormSubmit}>
+        <div class={`mb-3 ${styles.grid}`}>
+          <label for="exampleInputEmail1" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            name="email"
+            value={email}
+            required
+            onChange={(e) => setemail(e.target.value)}
+            aria-describedby="emailHelp"
+          />
+
+          <div class={`mb-3 ${styles.grid}`}>
+            <label for="exampleInputName1" class="form-label">
+              Name
+            </label>
+            <input
+              name="name"
+              type="text"
+              class="form-control"
+              value={name}
+              required
+              onChange={(e) => setname(e.target.value)}
+              id="exampleInputName1"
+            />
+          </div>
+          <div class={`mb-3 ${styles.grid}`}>
+            <label for="exampleInputTel1" class="form-label">
+              Phone
+            </label>
+            <input
+              name="phone"
+              type="tel"
+              class="form-control"
+              id="exampleInputTel1"
+              value={phone}
+              required
+              onChange={(e) => setphone(e.target.value)}
+            />
+          </div>
+          <div class={`mb-3 ${styles.grid}`}>
+            <label for="exampleInputDescription1" class="form-label">
+              Description
+            </label>
+            <textarea
+              onChange={(e) => setdesc(e.target.value)}
+              name="desc"
+              rows={6}
+              cols={21}
+              required
+            >
+              {desc}
+            </textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
--- utils/api.js
+++ utils/api.js
@@ -1,3 +1,4 @@
 export const host = "http://localhost:3000/";
 export const fetchBlogs = host + "api/blogs";
 export const fetchBlog = host + "api/getBlog?slug=";
+export const postContact = host + "api/postContact";
