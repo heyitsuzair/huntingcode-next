9083fe2b22c1204ea6af096a82934abcfc15f2a4
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-11-17 00:59:43 +0500

    About Page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,90 @@
 import React from "react";
+import styles from "../styles/About.module.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className={styles.grid}>
+      <h1>About Us</h1>
+      <h2>Introduction</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, explicabo
+        reiciendis unde consequatur repudiandae odit harum deleniti sequi qui
+        laudantium laboriosam, corporis minus, maxime dolorem dolore voluptatem
+        id odio? Id repudiandae aliquid suscipit quidem illo non quisquam
+        dolorum vero obcaecati ullam, ipsa atque inventore error quae. Doloribus
+        ipsa corporis excepturi? Facere nostrum voluptatum fugiat ipsa eos,
+        illum impedit animi totam quos, debitis optio. Delectus quidem illum
+        odio dolorum beatae nostrum nemo repellat quod ducimus sequi. Velit,
+        quis explicabo quae sit laudantium rerum, doloremque ipsa corporis ab
+        est adipisci accusamus? Fugit delectus laudantium ab dolore voluptatibus
+        hic, repudiandae debitis beatae a suscipit, cumque rem, eum pariatur
+        corrupti similique optio velit necessitatibus. Molestias doloribus
+        maxime consectetur dolorem sint recusandae nisi, accusamus neque
+        incidunt rerum dolor, explicabo magni unde? Nemo laborum consectetur
+        impedit quibusdam, itaque aliquid perferendis, asperiores sed iste
+        exercitationem dicta at? Ullam magni amet deleniti nesciunt unde nobis.
+        Fuga labore cupiditate nulla laborum officia? Suscipit consequuntur
+        itaque impedit fuga excepturi facere, nulla quibusdam dolores id
+        reiciendis! Id sint, libero eius assumenda, eligendi adipisci
+        repellendus delectus, reiciendis voluptatibus labore vitae. Consequatur
+        odit error, eius atque earum recusandae libero doloribus nihil vel ex
+        corporis, voluptatibus quas iure. Quam esse odit adipisci amet
+        repellendus?
+      </p>
+      <h2>Services</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, explicabo
+        reiciendis unde consequatur repudiandae odit harum deleniti sequi qui
+        laudantium laboriosam, corporis minus, maxime dolorem dolore voluptatem
+        id odio? Id repudiandae aliquid suscipit quidem illo non quisquam
+        dolorum vero obcaecati ullam, ipsa atque inventore error quae. Doloribus
+        ipsa corporis excepturi? Facere nostrum voluptatum fugiat ipsa eos,
+        illum impedit animi totam quos, debitis optio. Delectus quidem illum
+        odio dolorum beatae nostrum nemo repellat quod ducimus sequi. Velit,
+        quis explicabo quae sit laudantium rerum, doloremque ipsa corporis ab
+        est adipisci accusamus? Fugit delectus laudantium ab dolore voluptatibus
+        hic, repudiandae debitis beatae a suscipit, cumque rem, eum pariatur
+        corrupti similique optio velit necessitatibus. Molestias doloribus
+        maxime consectetur dolorem sint recusandae nisi, accusamus neque
+        incidunt rerum dolor, explicabo magni unde? Nemo laborum consectetur
+        impedit quibusdam, itaque aliquid perferendis, asperiores sed iste
+        exercitationem dicta at? Ullam magni amet deleniti nesciunt unde nobis.
+        Fuga labore cupiditate nulla laborum officia? Suscipit consequuntur
+        itaque impedit fuga excepturi facere, nulla quibusdam dolores id
+        reiciendis! Id sint, libero eius assumenda, eligendi adipisci
+        repellendus delectus, reiciendis voluptatibus labore vitae. Consequatur
+        odit error, eius atque earum recusandae libero doloribus nihil vel ex
+        corporis, voluptatibus quas iure. Quam esse odit adipisci amet
+        repellendus?
+      </p>
+      <h2>Programs Offered</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, explicabo
+        reiciendis unde consequatur repudiandae odit harum deleniti sequi qui
+        laudantium laboriosam, corporis minus, maxime dolorem dolore voluptatem
+        id odio? Id repudiandae aliquid suscipit quidem illo non quisquam
+        dolorum vero obcaecati ullam, ipsa atque inventore error quae. Doloribus
+        ipsa corporis excepturi? Facere nostrum voluptatum fugiat ipsa eos,
+        illum impedit animi totam quos, debitis optio. Delectus quidem illum
+        odio dolorum beatae nostrum nemo repellat quod ducimus sequi. Velit,
+        quis explicabo quae sit laudantium rerum, doloremque ipsa corporis ab
+        est adipisci accusamus? Fugit delectus laudantium ab dolore voluptatibus
+        hic, repudiandae debitis beatae a suscipit, cumque rem, eum pariatur
+        corrupti similique optio velit necessitatibus. Molestias doloribus
+        maxime consectetur dolorem sint recusandae nisi, accusamus neque
+        incidunt rerum dolor, explicabo magni unde? Nemo laborum consectetur
+        impedit quibusdam, itaque aliquid perferendis, asperiores sed iste
+        exercitationem dicta at? Ullam magni amet deleniti nesciunt unde nobis.
+        Fuga labore cupiditate nulla laborum officia? Suscipit consequuntur
+        itaque impedit fuga excepturi facere, nulla quibusdam dolores id
+        reiciendis! Id sint, libero eius assumenda, eligendi adipisci
+        repellendus delectus, reiciendis voluptatibus labore vitae. Consequatur
+        odit error, eius atque earum recusandae libero doloribus nihil vel ex
+        corporis, voluptatibus quas iure. Quam esse odit adipisci amet
+        repellendus?
+      </p>
+    </div>
+  );
 };
 
 export default About;
